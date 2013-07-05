f7b3d57309a78564923349c5fc6b2d2fe3cdec8e
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2013-07-05 14:15:52 +0200

    Added test for the execute() method

--- watchtower/test_service.py
+++ watchtower/test_service.py
@@ -1,5 +1,5 @@
 """ Testing the Service class """
-from nose.tools import assert_in, assert_not_in
+from nose.tools import assert_equal, assert_in, assert_not_in
 
 from watchtower import service
 
@@ -10,8 +10,8 @@ class TestService(service.Service):
     def __init__(self):
         super(TestService, self).__init__('test')
 
-    def test_add(self):
-        """ Test the add function """
+    def test_add_monitor_to_list(self):
+        """ Check that monitors are added to the monitor list """
         name = 'Disk space - /'
 
         def check_disk(rtn, disk='/'):
@@ -29,3 +29,23 @@ class TestService(service.Service):
             args=['ok'])
         assert_in(name, self.monitors)
 
+    def test_execute(self):
+        """ Basic execution test """
+        name = 'Disk space - /'
+
+        def check_disk(rtn, disk='/'):
+            return {
+                'status': 0,
+                'message': 'All is OK'
+            }
+
+        self.add(
+            name=name,
+            monitor=check_disk,
+            interval=1,
+            args='a')
+
+        result = self.execute(name)
+        assert result
+        assert_equal(0, result['status'])
+        assert_equal('All is OK', result['message'])
